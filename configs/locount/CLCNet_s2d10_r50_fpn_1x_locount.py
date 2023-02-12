66de81e2e3b126d0841a0aacb388420621a1f302
SakiRinn <sakirinn@qq.com>
2023-02-13 03:44:36 +0800

    edit configs.

--- configs/locount/CLCNet_s2d10_r50_fpn_1x_locount.py
+++ configs/locount/CLCNet_s2d10_r50_fpn_1x_locount.py
@@ -47,7 +47,7 @@ model = dict(
                 bbox_coder=dict(
                     type='DeltaXYWHBBoxCoder',
                     target_means=[0., 0., 0., 0.],
-                    target_stds=[0.05, 0.05, 0.1, 0.1]),
+                    target_stds=[0.033, 0.033, 0.067, 0.067]),
                 reg_class_agnostic=True,
                 loss_cls=dict(
                     type='CrossEntropyLoss',
@@ -62,5 +62,41 @@ model = dict(
                     use_sigmoid=False,
                     loss_weight=1.0)),
         ]
-    )
+    ),
+    train_cfg=dict(
+        rcnn=[
+            dict(
+                assigner=dict(
+                    type='MaxIoUAssignerWithCount',
+                    pos_iou_thr=0.5,
+                    neg_iou_thr=0.5,
+                    min_pos_iou=0.5,
+                    match_low_quality=False,
+                    ignore_iof_thr=-1),
+                sampler=dict(
+                    type='RandomSamplerWithCount',
+                    num=512,
+                    pos_fraction=0.25,
+                    neg_pos_ub=-1,
+                    add_gt_as_proposals=True),
+                pos_weight=-1,
+                debug=False),
+            dict(
+                assigner=dict(
+                    type='MaxIoUAssignerWithCount',
+                    pos_iou_thr=0.7,
+                    neg_iou_thr=0.7,
+                    min_pos_iou=0.7,
+                    match_low_quality=False,
+                    ignore_iof_thr=-1),
+                sampler=dict(
+                    type='RandomSamplerWithCount',
+                    num=512,
+                    pos_fraction=0.25,
+                    neg_pos_ub=-1,
+                    add_gt_as_proposals=True),
+                pos_weight=-1,
+                debug=False)
+        ]
+    ),
 )
